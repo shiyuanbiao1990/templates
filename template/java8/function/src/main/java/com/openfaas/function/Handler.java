359946b7b43d399e7186254bd5afb3e3da4205de
shiyuanbiao <shiyuanbiao1990@126.com>
2019-01-02 17:06:39 +0800

    修改官方java8模板

--- template/java8/function/src/main/java/com/openfaas/function/Handler.java
+++ template/java8/function/src/main/java/com/openfaas/function/Handler.java
@@ -9,7 +9,7 @@ public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
         Response res = new Response();
-	    res.setBody("Hello, world!");
+	    res.setBody("Hello, I am syb!");
 
 	    return res;
     }
