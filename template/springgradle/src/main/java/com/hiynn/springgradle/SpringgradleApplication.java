b1b4bad5bef855b444dc7460212c0e08ab57da68
shiyuanbiao1990 <44049244+shiyuanbiao1990@users.noreply.github.com>
2019-01-02 16:19:51 +0800

    Add files via upload

---
+++ template/springgradle/src/main/java/com/hiynn/springgradle/SpringgradleApplication.java
@@ -0,0 +1,14 @@
+package com.hiynn.springgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgradleApplication.class, args);
+	}
+
+}
+
---
+++ template/springgradle/src/test/java/com/hiynn/springgradle/SpringgradleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hiynn.springgradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringgradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
