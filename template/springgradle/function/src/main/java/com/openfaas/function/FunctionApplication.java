4b774d1fa721c9be7de55fb28f618a37423d0cc7
shiyuanbiao1990 <44049244+shiyuanbiao1990@users.noreply.github.com>
2019-01-03 17:53:54 +0800

    Add files via upload

---
+++ template/springgradle/function/src/main/java/com/openfaas/function/FunctionApplication.java
@@ -0,0 +1,14 @@
+package com.openfaas.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionApplication.class, args);
+	}
+
+}
+
