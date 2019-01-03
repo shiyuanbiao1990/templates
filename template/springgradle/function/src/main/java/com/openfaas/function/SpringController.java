7ec7bc1c9d9d58ece618a1b90bc86120c2b3845f
shiyuanbiao1990 <44049244+shiyuanbiao1990@users.noreply.github.com>
2019-01-03 15:33:43 +0800

    Add files via upload

---
+++ template/springgradle/function/src/main/java/com/openfaas/function/SpringController.java
@@ -0,0 +1,17 @@
+package com.openfaas.function;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: 石远彪
+ * @Date: 2019/1/2 16:12
+ * @Description:
+ */
+@RestController
+public class SpringController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";//返回结果为字符串
+    }
+}
