8ba2c4f05c32cddc1532789820d5db42e3aff024
shiyuanbiao <shiyuanbiao1990@126.com>
2019-01-08 15:05:07 +0800

    添加java8环境变量

--- template/java8/function/src/main/java/com/openfaas/function/Handler.java
+++ template/java8/function/src/main/java/com/openfaas/function/Handler.java
@@ -8,8 +8,10 @@ import com.openfaas.model.Response;
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
+        String  result = System.getenv("MESSAGE");
         Response res = new Response();
-	    res.setBody("Hello, this is syb!");
+
+	    res.setBody(result);
 
 	    return res;
     }
